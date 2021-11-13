9e05ff6b75c0059316a3cf9380bf0592acabe7c2
iPowers <isika.powers@gmail.com>
2021-11-12 20:04:53 -0700

    Create forecast controller.

---
+++ app/controllers/api/v1/forecast_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::ForecastController < ApplicationController
+  def index
+    if params[:location].present?
+      forecast = ForecastFacade.get_forecast(params[:location])
+
+      render json: ForecastSerializer.new(forecast), status: :ok
+
+    else
+      render json: { error: 'valid params not given' }, status: :bad_request
+    end
+  end
+end
