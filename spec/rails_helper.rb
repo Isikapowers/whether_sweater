ec3dbc3f94236d8a203fa22b6c86f27e960d4d85
iPowers <isika.powers@gmail.com>
2021-11-13 16:15:13 -0700

    Add config.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,9 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'webmock/rspec'
 require 'simplecov'
+require 'factory_bot_rails'
+
 SimpleCov.start
 
 ENV['RAILS_ENV'] ||= 'test'
@@ -62,6 +65,25 @@ RSpec.configure do |config|
 
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
+  config.include FactoryBot::Syntax::Methods
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+  config.allow_http_connections_when_no_cassette = true
+  config.filter_sensitive_data('<appid>') { ENV['OPEN_WEATHER_KEY'] }
+  config.filter_sensitive_data('<key>') { ENV['MAPQUEST_KEY'] }
+  config.filter_sensitive_data('<client_id>') { ENV['UNSPLASH_KEY'] }
+  config.default_cassette_options = { re_record_interval: 7.days }
+  config.configure_rspec_metadata!
 end
