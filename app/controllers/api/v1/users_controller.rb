5a262983d2f7feab69e539bd27eafd650802d111
iPowers <isika.powers@gmail.com>
2021-11-14 12:57:08 -0700

    Delete index action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,15 +1,8 @@
 class Api::V1::UsersController < ApplicationController
-  def index
-    users = User.all
-    users.each do |user|
-      render json: UsersSerializer.new(user)
-    end
-  end
-
   def create
     user = User.new(user_params)
     if user.save
-      user.update(api_key: SecureRandom.hex(20))
+      # user.update(api_key: SecureRandom.hex(20))
       render json: UsersSerializer.new(user), status: :created
     else
       render json: { error: user.errors.full_messages }, status: :unprocessable_entity
