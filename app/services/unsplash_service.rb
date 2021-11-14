6178f9aeeefffffb3378c15bc02e2d04e8ef67b6
iPowers <isika.powers@gmail.com>
2021-11-14 13:03:12 -0700

    Fix key name.

--- app/services/unsplash_service.rb
+++ app/services/unsplash_service.rb
@@ -1,7 +1,7 @@
 class UnsplashService < BaseService
   def self.search_images(location)
     response = Faraday.get('https://api.unsplash.com/search/photos') do |req|
-      req.params['client_id'] = ENV['UNSLASH_KEY']
+      req.params['client_id'] = ENV['UNSPLASH_KEY']
       req.params['query'] = location.downcase
       req.params['page'] = 1
       req.headers['Content-Type'] = 'application/json'
