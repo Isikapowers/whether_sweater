d8fedeedc58b47d8dc2cfbac49e71ea9e7faef8f
iPowers <isika.powers@gmail.com>
2021-11-15 10:52:19 -0700

    Create activities serializer

---
+++ app/serializers/activities_serializer.rb
@@ -0,0 +1,31 @@
+class ActivitiesSerializer
+  def self.format_data(activities, weather, destination)
+    {
+      "data": {
+        "id": "null",
+        "type": "activities",
+        "attributes": {
+          "destination": "#{destination}",
+          "forecast": {
+            "summary": "#{weather.current_weather[:condition]}",
+            "temperature": "#{weather.current_weather[:temperature]} F"
+          },
+          "activities": [
+          {
+            "title": activities.first.activity,
+            "type": activities.first.type,
+            "participants": activities.first.participants,
+            "price": activities.first.price
+          },
+          {
+            "title": activities.second.activity,
+            "type": activities.second.type,
+            "participants": activities.second.participants,
+            "price": activities.second.price
+          }
+          ]
+        }
+      }
+    }
+  end
+end
